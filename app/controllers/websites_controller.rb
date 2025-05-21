71f24f2677b08291ebf268b11be8a2532a378b08
Alexander6380727 <alexander.oga@student.mahidol.edu>
2025-05-21 20:10:29 +0700

    Website Model editing

--- app/controllers/websites_controller.rb
+++ app/controllers/websites_controller.rb
@@ -1,2 +1,39 @@
 class WebsitesController < ApplicationController
+    
+    def index
+        @websites = Website.all
+    end
+
+    def show
+        @website = Website.find(params[:id])
+    end
+
+    def new
+        @website = Website.new
+    end
+
+    def create
+        @website = Website.new(website_params)
+        if @website.save
+            redirect_to @website, notice: 'Website was successfully created.'
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    def edit
+        @website = Website.find(params[:id])
+    end
+
+    def update
+        @website = Website.find(params[:id])
+        if @website.update(website_params)
+            redirect_to @website, notice: 'Website was successfully updated.'
+        else
+            render :edit, status: :unprocessable_entity
+
+    private
+    def website_params
+        params.expect(website: [:name, :url])
+    end
 end
