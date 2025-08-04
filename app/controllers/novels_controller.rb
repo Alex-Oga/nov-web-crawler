aec17bb0864d0d169b2307aeedb779ac8fb6578d
Alexander6380727 <alexander.oga@student.mahidol.edu>
2025-08-04 21:01:27 +0700

    Novel access routing

--- app/controllers/novels_controller.rb
+++ app/controllers/novels_controller.rb
@@ -1,21 +1,21 @@
 class NovelsController < ApplicationController
     allow_unauthenticated_access
-    before_action :set_novel
+    before_action :set_website, only: [:index, :new, :create]
+    before_action :set_novel, only: [:show, :edit, :update]
 
     def index
-        @novels = Novel.all
+        @novels = @website.novels
     end
 
     def show
-        @novel = Novel.find(params[:id])
     end
 
     def new
-        @novel = Novel.new
+        @novel = @website.novels.build
     end
 
     def create
-        @novel = Novel.new(novel_params)
+        @novel = @website.novel.build(novel_params)
         if @novel.save
             redirect_to @novel, notice: "New Novel Registered."
         else
@@ -23,9 +23,25 @@ class NovelsController < ApplicationController
         end
     end
 
+    def edit
+    end
+
+  def update
+    if @novel.update(novel_params)
+      redirect_to @novel, notice: "Novel was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
     private
 
+    def set_website
+        @website = Website.find(params[:website_id])
+    end
+
     def set_novel
+        @novel = Novel.find(params[:id])
     end
 
     def novel_params
